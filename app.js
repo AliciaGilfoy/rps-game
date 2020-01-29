c9bab5f31fa9f0046b40d8fd18b7074363cb02e4
Alicia Gilfoy <aliciagilfoy@gmail.com>
2020-01-29 14:30:08 -0700

    html and css

---
+++ app.js
@@ -0,0 +1,47 @@
+let choices = [
+  {
+    name: "rock",
+    img: "./rock.png",
+  },
+  {
+    name: "paper",
+    img: "./paper.png",
+  },
+  {
+    name: "scissors",
+    img: "./scissors",
+  },
+  {
+    name: "ready",
+    img: "./fist.png",
+  }
+]
+
+function pickRock() {
+
+}
+
+function pickPaper() {
+
+}
+
+function pickScissors() {
+
+}
+
+function pickRandom() {
+
+}
+
+function drawMatch() {
+
+}
+
+function drawWinner() {
+
+}
+
+function resetMatch() {
+
+}
+
