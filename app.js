00d446805128e79348dcee24d5c5b3a3ed879196
Alicia Gilfoy <aliciagilfoy@gmail.com>
2020-01-29 22:37:33 -0700

    finished

--- app.js
+++ app.js
@@ -27,11 +27,17 @@ let players = [
   },
 ]
 
-let computerPickStatusElem = document.getElementById("computer-choice")
+let computerPickStatusElem = document.getElementById("computer-choice").textContent
 let computerPickImageElem = document.getElementById("computer-img")
 let playerPickImageElem = document.getElementById("player-img")
-let playerOneChoice = "ready"
-let computerPick = "ready"
+let playerPickStatusElem = document.getElementById("player-choice").textContent
+// let playerOneChoice = "ready"
+// let computerPick = "ready"
+let playerScore = players[0].score
+let computerScore = players[1].score
+
+// let playerScore = document.getElementById("player-one-score").toString()
+// let computerScore = document.getElementById("computer-score").toString()
 
 // on rock button click this brings up rock picture
 function pickRock() {
@@ -40,7 +46,11 @@ function pickRock() {
   let playerOnePicture = str1.concat(playerOneChoice)
   console.log(playerOneChoice)
   document.getElementById("player-img").classList.replace("card-image-top-ready", playerOnePicture)
+  document.getElementById("player-choice").textContent = playerOneChoice
+  playerPickStatusElem = playerOneChoice
   drawMatch()
+  drawWinner()
+  // drawWinner(playerPickStatusElem, computerPickStatusElem)
 }
 // on paper button click this brings up paper picture
 function pickPaper() {
@@ -49,7 +59,11 @@ function pickPaper() {
   let playerOnePicture = str1.concat(playerOneChoice)
   console.log(playerOneChoice)
   document.getElementById("player-img").classList.replace("card-image-top-ready", playerOnePicture)
+  document.getElementById("player-choice").textContent = playerOneChoice
+  playerPickStatusElem = playerOneChoice
   drawMatch()
+  drawWinner()
+  // drawWinner(playerPickStatusElem, computerPickStatusElem)
 }
 // on scissors button click this brings up scissors picture
 function pickScissors() {
@@ -58,7 +72,11 @@ function pickScissors() {
   let playerOnePicture = str1.concat(playerOneChoice)
   console.log(playerOneChoice)
   document.getElementById("player-img").classList.replace("card-image-top-ready", playerOnePicture)
+  document.getElementById("player-choice").textContent = playerOneChoice
+  playerPickStatusElem = playerOneChoice
   drawMatch()
+  drawWinner()
+  // drawWinner(playerPickStatusElem, computerPickStatusElem)
 }
 // on undecided button click this brings up a random picture
 function pickRandom() {
@@ -68,7 +86,11 @@ function pickRandom() {
   let randomPicture = str1.concat(playerOneChoice)
   console.log(playerOneChoice)
   document.getElementById("player-img").classList.replace("card-image-top-ready", randomPicture)
+  document.getElementById("player-choice").textContent = playerOneChoice
+  playerPickStatusElem = playerOneChoice
   drawMatch()
+  drawWinner()
+  // drawWinner(playerPickStatusElem, computerPickStatusElem)
 }
 // after any player 1 button is clicked this makes the computer pick a random picture
 function drawMatch() {
@@ -79,11 +101,79 @@ function drawMatch() {
   console.log(computerPick)
   document.getElementById("computer-img").classList.replace("card-image-top-ready", randomPicture)
   document.getElementById("computer-choice").textContent = computerPick
+  computerPickStatusElem = computerPick
 }
+
+
 // this determines the winner
 function drawWinner() {
-
+  if ((document.getElementById("player-choice").textContent === "rock") && (document.getElementById("computer-choice").textContent === "scissors")) {
+    document.getElementById("match-result").textContent = "YOU WIN!";
+    playerScore++
+  }
+  if ((document.getElementById("player-choice").textContent === "rock") && (document.getElementById("computer-choice").textContent === "paper")) {
+    document.getElementById("match-result").textContent = "YOU LOSE."
+    computerScore++
+  }
+  if ((document.getElementById("player-choice").textContent === "rock") && (document.getElementById("computer-choice").textContent === "rock")) {
+    document.getElementById("match-result").textContent = "DRAW"
+  }
+  if ((document.getElementById("player-choice").textContent === "paper") && (document.getElementById("computer-choice").textContent === "scissors")) {
+    document.getElementById("match-result").textContent = "YOU LOSE."
+    computerScore++
+  }
+  if ((document.getElementById("player-choice").textContent === "paper") && (document.getElementById("computer-choice").textContent === "rock")) {
+    document.getElementById("match-result").textContent = "YOU WIN!"
+    playerScore++
+  }
+  if ((document.getElementById("player-choice").textContent === "paper") && (document.getElementById("computer-choice").textContent === "paper")) {
+    document.getElementById("match-result").textContent = "DRAW";
+  }
+  if ((document.getElementById("player-choice").textContent === "scissors") && (document.getElementById("computer-choice").textContent === "scissors")) {
+    document.getElementById("match-result").textContent = "DRAW"
+  }
+  if ((document.getElementById("player-choice").textContent === "scissors") && (document.getElementById("computer-choice").textContent === "paper")) {
+    document.getElementById("match-result").textContent = "YOU WIN!"
+    playerScore++
+  }
+  if ((document.getElementById("player-choice").textContent === "scissors") && (document.getElementById("computer-choice").textContent === "rock")) {
+    document.getElementById("match-result").textContent = "YOU LOSE."
+    computerScore++
+  }
+  keepScore()
 }
+
+//   for (playerPickStatusElem, computerPickStatusElem) {
+//     case ("rock" && "scissors"):
+//       return document.getElementById("match-result").textContent = "YOU WIN!";
+//       break;
+//     case ("rock" && "paper"):
+//       return document.getElementById("match-result").textContent = "YOU LOSE.";
+//       break;
+//     case ("rock" && "rock"):
+//       return document.getElementById("match-result").textContent = "DRAW";
+//       break;
+//     case ("paper" && "scissors"):
+//       return document.getElementById("match-result").textContent = "YOU LOSE.";
+//       break;
+//     case ("paper" && "rock"):
+//       return document.getElementById("match-result").textContent = "YOU WIN!";
+//       break;
+//     case ("paper" && "paper"):
+//       return document.getElementById("match-result").textContent = "DRAW";
+//       break;
+//     case ("scissors" && "scissors"):
+//       return document.getElementById("match-result").textContent = "DRAW";
+//       break;
+//     case ("scissors" && "paper"):
+//       return document.getElementById("match-result").textContent = "YOU WIN!";
+//       break;
+//     case ("scissors" && "rock"):
+//       return document.getElementById("match-result").textContent = "YOU LOSE.";
+//       break;
+//   }
+// }
+
 // after clicking play again the game resets
 function resetMatch() {
   document.getElementById("computer-img").classList = ""
@@ -91,8 +181,11 @@ function resetMatch() {
   document.getElementById("player-img").classList = ""
   document.getElementById("player-img").classList.add("card-image-top-ready")
   document.getElementById("computer-choice").textContent = ""
+  document.getElementById("player-choice").textContent = ""
+  document.getElementById("match-result").textContent = ""
 }
 // this keeps score after each match
 function keepScore() {
-
+  document.getElementById("player-one-score").textContent = playerScore.toString()
+  document.getElementById("computer-score").textContent = computerScore.toString()
 }
